a23170ce28665ff77614c2e6a12338c100d684d2
Thomas Hourdel <chman.a11w@gmail.com>
2014-10-29 12:54:00 +0100

    Added "Typogenic Manual" to the help menu for quick doc access

---
+++ Typogenic/Editor/TypogenicHelpMenu.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class TypogenicHelpMenu : Editor
+{
+	static string pathManual = "/Typogenic/Documentation/index.html";
+
+	[MenuItem("Help/Typogenic Manual", false, 0)]
+	public static void MenuManual()
+	{
+		Application.OpenURL("file://" + Application.dataPath + pathManual);
+	}
+}
